5bf549f0e51a38045ea5624ba19fa6bd3cc15a62
sturdytea <aitmazh@gmail.com>
2026-06-26 22:06:34 +0500

    Initial Commit

---
+++ ObjectScanner/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//
+// ContentView.swift
+// ObjectScanner
+//
+// Created by sturdytea on 26.06.2026.
+//
+// GitHub: https://github.com/sturdytea
+//
+    
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ObjectScanner/ObjectScannerApp.swift
@@ -0,0 +1,21 @@
+//
+//
+// ObjectScannerApp.swift
+// ObjectScanner
+//
+// Created by sturdytea on 26.06.2026.
+//
+// GitHub: https://github.com/sturdytea
+//
+    
+
+import SwiftUI
+
+@main
+struct ObjectScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
