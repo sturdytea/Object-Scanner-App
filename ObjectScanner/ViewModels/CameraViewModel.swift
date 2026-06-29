3e63fdcb47ca48ea7f944dfc39e4306ff5d1f6a3
sturdytea <aitmazh@gmail.com>
2026-06-29 21:47:55 +0500

    first commit

---
+++ ObjectScanner/ViewModels/CameraViewModel.swift
@@ -0,0 +1,12 @@
+//
+//
+// CameraViewModel.swift
+// ObjectScanner
+//
+// Created by sturdytea on 29.06.2026.
+//
+// GitHub: https://github.com/sturdytea
+//
+    
+
+import Foundation
---
+++ ObjectScanner/Views/CameraView.swift
@@ -0,0 +1,22 @@
+//
+//
+// CameraView.swift
+// ObjectScanner
+//
+// Created by sturdytea on 29.06.2026.
+//
+// GitHub: https://github.com/sturdytea
+//
+    
+
+import SwiftUI
+
+struct CameraView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraView()
+}
